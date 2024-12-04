05722b44417cf61dcf600af1b2ca9c2c06a9378c
mRcOol7 <196450307014@kjpbharuch.gujgov.edu.in>
2024-12-04 10:21:22 +0530

    ..

--- project/src/api.js
+++ project/src/api.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-const baseURL = process.env.REACT_APP_API_URL_PRODUCTION || 'http://localhost:5000/'; 
-
 export const api = axios.create({
-  baseURL, 
-  timeout: 5000, 
+    // baseURL: "http://localhost:5000/",
+    baseURL: "https://backend-sigma-orpin.vercel.app/",
 });
 
 export default api;

3cd69086e63878aaa2626d0b9fbf13941817e82c
mRcOol7 <196450307014@kjpbharuch.gujgov.edu.in>
2024-12-04 10:30:41 +0530

    ..

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,12 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// CORS Configuration to allow all origins
 const corsOptions = {
-    origin: ['https://internship-esg.vercel.app', 'http://localhost:3000'], 
+    origin: '*', // Allow all origins
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
+    credentials: false, // Credentials cannot be used with '*'
 };
 
 app.use(cors(corsOptions));
@@ -22,6 +23,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.get('/favicon.png', (req, res) => res.status(204).end());
+app.get('/favicon.ico', (req, res) => res.status(204).end());
 app.get('/', (req, res) => res.send('Hello, Vercel!'));
 
 app.use('/api', authRoutes);
